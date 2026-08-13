3b59eb8f9ad2123a7b1849e9ffc6fecc604c7802
SamsonVadny <samsonvadny28@gmail.com>
2026-08-13 20:57:41 +0200

    Change load function to load_to_db

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from etl.extract import extract_product, extract_users
 from etl.transform import transform_products, transform_users
-from etl.load import load_to_postgres
+from etl.load import load_to_db
 
 def run_pipeline():
     print('Starting Pipeline')
@@ -13,10 +13,10 @@ def run_pipeline():
     product_df = transform_products(product_df)
     user_df = transform_users(user_df)
 
-    print('Starting to Postgres')
+    print('Starting to DB')
 
-    load_to_postgres(product_df, 'products')
-    load_to_postgres(user_df, 'users')
+    load_to_db(product_df, 'products')
+    load_to_db(user_df, 'users')
 
     print('ETL Pipeline Completed')
 
